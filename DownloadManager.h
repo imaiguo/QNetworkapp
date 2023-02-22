755476d61f04797bca0b50a31338bb76779c5892
Ma Aiguo <imaiguo@gmail.com>
2023-02-22 16:42:43 +0800

    update pro name

---
+++ DownloadManager.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <QtCore>
+#include <QtNetwork>
+
+#include <cstdio>
+
+QT_BEGIN_NAMESPACE
+class QSslError;
+QT_END_NAMESPACE
+
+using namespace std;
+
+class DownloadManager: public QObject
+{
+    Q_OBJECT
+    QNetworkAccessManager manager;
+    QVector<QNetworkReply *> currentDownloads;
+
+public:
+    DownloadManager();
+    void doDownload(const QUrl &url);
+    static QString saveFileName(const QUrl &url);
+    bool saveToDisk(const QString &filename, QIODevice *data);
+    static bool isHttpRedirect(QNetworkReply *reply);
+
+public slots:
+    void execute();
+    void downloadFinished(QNetworkReply *reply);
+    void sslErrors(const QList<QSslError> &errors);
+};
---
+++ qget.pro
@@ -0,0 +1,10 @@
+QT = core network
+CONFIG += cmdline
+
+HEADERS   += DownloadManager.h
+
+SOURCES += main.cpp DownloadManager.cc
+
+# install
+target.path = C:/QtProc/
+INSTALLS += target
